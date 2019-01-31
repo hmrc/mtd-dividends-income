31c001a50cbd85077cee5709aaad89592158bdb0
Jacob Brownlow <35334115+jacobbrownlow@users.noreply.github.com>
2019-01-31 15:56:01 +0000

    MTD-API Giter8 Template (#2)
    * MTD-API Giter8 template
    
    * add dependancies
    
    * add Agent Auth fix
    
    * add Agent Auth fix tests
    
    * remove init file

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-
 # mtd-dividends-income
 
-This is a placeholder README.md for a new repository
+[![Apache-2.0 license](http://img.shields.io/badge/license-Apache-brightgreen.svg)](http://www.apache.org/licenses/LICENSE-2.0.html)
+[![Download](https://api.bintray.com/packages/hmrc/releases/mtd-dividends-income/images/download.svg) ](https://bintray.com/hmrc/releases/mtd-dividends-income/_latestVersion)
+
+This service manages the mtd-dividends-income.
 
 ### License
 
-This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
+This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html")
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,21 +1,41 @@
-import play.core.PlayVersion.current
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import play.core.PlayVersion
 import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
   val compile = Seq(
-
-    "uk.gov.hmrc"             %% "bootstrap-play-25"        % "4.8.0"
+    ws,
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.0.0",
+    "uk.gov.hmrc" %% "domain" % "5.2.0",
+    "org.typelevel" %% "cats-core" % "1.1.0"
   )
 
-  val test = Seq(
-    "org.scalatest"           %% "scalatest"                % "3.0.4"                 % "test",
-    "com.typesafe.play"       %% "play-test"                % current                 % "test",
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "uk.gov.hmrc"             %% "service-integration-test" % "0.2.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.0"                 % "test, it"
-  )
+  def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
+    "org.scalatest" %% "scalatest" % "3.0.4" % scope,
+    "org.scalamock" %% "scalamock" % "4.1.0" % scope,
+    "org.pegdown" % "pegdown" % "1.6.0" % scope,
 
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+
+    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % scope,
+
+    "com.github.tomakehurst" % "wiremock" % "2.6.0" % scope
+  )
 }
