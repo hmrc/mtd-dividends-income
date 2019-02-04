e138eb554f14b16b81b7304289cfd307c928719b
MeliaDavies <39948809+MeliaDavies@users.noreply.github.com>
2019-02-04 13:31:57 +0000

    Created DES response parser and connector

---
+++ test/v2/connectors/DesConnectorSpec.scala
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.connectors
+
+import uk.gov.hmrc.domain.Nino
+import v2.fixtures.Fixtures.DividendsFixture
+import v2.mocks.{MockAppConfig, MockHttpClient}
+import v2.models.Dividends
+import v2.models.errors.{MultipleErrors, NinoFormatError, SingleError, TaxYearFormatError}
+import v2.models.outcomes.{AmendDividendsConnectorOutcome, DesResponse}
+import v2.models.requestData.{AmendDividendsRequest, DesTaxYear}
+
+import scala.concurrent.Future
+
+class DesConnectorSpec extends ConnectorSpec {
+
+  trait Test extends MockHttpClient with MockAppConfig {
+    val connector = new DesConnector(
+      http = mockHttpClient,
+      appConfig = mockAppConfig
+    )
+    MockedAppConfig.desBaseUrl returns baseUrl
+    MockedAppConfig.desToken returns "des-token"
+    MockedAppConfig.desEnvironment returns "des-environment"
+  }
+
+  lazy val baseUrl = "test-BaseUrl"
+
+  "calling amend" should {
+    "return successful response" when {
+      "valid data is supplied" in new Test {
+        val nino = "AA123456A"
+        val taxYear = "2018-19"
+        val desTaxYear = "2019"
+        val amendDividendsRequest = AmendDividendsRequest(Nino(nino), desTaxYear, DividendsFixture.dividendsModel)
+        val correlationId = "X-123"
+        val transactionReference = "000000000001"
+        val expectedResult = DesResponse[String](correlationId, transactionReference)
+
+        MockedHttpClient.post[Dividends, AmendDividendsConnectorOutcome](
+          s"$baseUrl" + s"/income-tax/nino/$nino/income-source/dividends/annual/$desTaxYear",
+          DividendsFixture.dividendsModel)
+          .returns(Future.successful(Right(expectedResult)))
+
+        val result = await(connector.amend(amendDividendsRequest))
+        result shouldBe Right(expectedResult)
+      }
+    }
+
+    "return error response with correlationId and tax year format error" when {
+      "the request supplied has invalid tax year" in new Test() {
+
+        val correlationId = "X-123"
+        val expectedDesResponse = DesResponse(correlationId, SingleError(TaxYearFormatError))
+        val nino = "AA123456A"
+        val taxYear = "2018-19"
+
+        MockedHttpClient.post[Dividends, AmendDividendsConnectorOutcome](
+          s"$baseUrl/income-tax/nino/$nino/income-source/dividends/annual/${DesTaxYear(taxYear)}",
+          DividendsFixture.dividendsModel)
+          .returns(Future.successful(Left(expectedDesResponse)))
+
+        val amendDividendsRequest = AmendDividendsRequest(Nino(nino), DesTaxYear(taxYear), DividendsFixture.dividendsModel)
+
+        val result = await(connector.amend(amendDividendsRequest))
+
+        result shouldBe Left(expectedDesResponse)
+      }
+    }
+
+    "return response with multiple errors and correlationId" when {
+      "an request supplied with invalid tax year and invalid Nino " in new Test() {
+
+        val correlationId = "X-123"
+        val expectedDesResponse = DesResponse(correlationId, MultipleErrors(Seq(TaxYearFormatError, NinoFormatError)))
+        val nino = "AA123456A"
+        val taxYear = "2018-19"
+
+        MockedHttpClient.post[Dividends, AmendDividendsConnectorOutcome](
+          s"$baseUrl/income-tax/nino/$nino/income-source/dividends/annual/${DesTaxYear(taxYear)}",
+          DividendsFixture.dividendsModel)
+          .returns(Future.successful(Left(expectedDesResponse)))
+
+        val amendDividendsRequest = AmendDividendsRequest(Nino(nino), DesTaxYear(taxYear), DividendsFixture.dividendsModel)
+
+        val result = await(connector.amend(amendDividendsRequest))
+
+        result shouldBe Left(expectedDesResponse)
+      }
+    }
+  }
+}
---
+++ test/v2/connectors/httpparsers/AmendDividendsHttpParserSpec.scala
@@ -0,0 +1,207 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.connectors.httpparsers
+
+import play.api.libs.json.{JsValue, Json}
+import play.api.test.Helpers._
+import support.UnitSpec
+import uk.gov.hmrc.http.HttpResponse
+import v2.models.errors._
+import v2.models.outcomes.DesResponse
+
+class AmendDividendsHttpParserSpec extends UnitSpec {
+
+  val method = "PUT"
+  val url = "test-url"
+
+  val transactionReference = "000000000001"
+  val desExpectedJson: JsValue = Json.obj("transactionReference" -> transactionReference)
+  val desResponse = DesResponse("X-123", transactionReference)
+
+  val correlationId = "X-123"
+
+  "read" should {
+    "return a DesResponse" when {
+      "the http response contains a 200" in {
+
+        val httpResponse = HttpResponse(OK, Some(desExpectedJson), Map("CorrelationId" -> Seq(correlationId)))
+
+        val result = AmendDividendsHttpParser.amendHttpReads.read(PUT, "/test", httpResponse)
+        result shouldBe Right(desResponse)
+      }
+    }
+
+    "return a single error" when {
+      "the http response contains a 400 with an error response body" in {
+        val errorResponseJson = Json.parse(
+          """
+            |{
+            |  "code": "INVALID_NINO",
+            |  "reason": "some reason"
+            |}
+          """.stripMargin)
+        val expected = DesResponse(correlationId, SingleError(MtdError("INVALID_NINO", "some reason")))
+
+        val httpResponse = HttpResponse(BAD_REQUEST, Some(errorResponseJson), Map("CorrelationId" -> Seq(correlationId)))
+        val result = AmendDividendsHttpParser.amendHttpReads.read(PUT, "/test", httpResponse)
+        result shouldBe Left(expected)
+      }
+
+      "the http response contains a 403 with an error response body" in {
+        val errorResponseJson = Json.parse(
+          """
+            |{
+            |  "code": "MISSING_GIFT_AID_AMOUNT",
+            |  "reason": "some reason"
+            |}
+          """.stripMargin)
+        val expected =  DesResponse(correlationId, SingleError(MtdError("MISSING_GIFT_AID_AMOUNT", "some reason")))
+
+        val httpResponse = HttpResponse(FORBIDDEN, Some(errorResponseJson), Map("CorrelationId" -> Seq(correlationId)))
+        val result = AmendDividendsHttpParser.amendHttpReads.read(PUT, "/test", httpResponse)
+        result shouldBe Left(expected)
+      }
+    }
+
+    "return a generic error" when {
+
+      "the successful json response from DES can't be read" in {
+
+        val httpResponse = HttpResponse(OK, Some(Json.obj("foo" -> "bar")), Map("CorrelationId" -> Seq(correlationId)))
+        val expected =  DesResponse(correlationId, GenericError(DownstreamError))
+
+
+        val result = AmendDividendsHttpParser.amendHttpReads.read(PUT, "/test", httpResponse)
+        result shouldBe Left(expected)
+      }
+
+      "the error response from DES can't be read" in {
+        val errorResponseJson = Json.parse(
+          """
+            |{
+            |  "foo": "TEST_CODE",
+            |  "bar": "some reason"
+            |}
+          """.
+            stripMargin)
+        val expected =  DesResponse(correlationId, GenericError(DownstreamError))
+
+        val httpResponse = HttpResponse(BAD_REQUEST, Some(errorResponseJson), Map("CorrelationId" -> Seq(correlationId)))
+        val result = AmendDividendsHttpParser.amendHttpReads.read(PUT, "/test", httpResponse)
+        result shouldBe Left(expected)
+      }
+
+      "the error response status code is not one that is handled" in {
+        val errorResponseJson = Json.parse(
+          """
+            |{
+            |  "foo": "TEST_CODE",
+            |  "bar": "some reason"
+            |}
+          """.
+            stripMargin)
+        val expected =  DesResponse(correlationId, GenericError(DownstreamError))
+        val unHandledStatusCode = SEE_OTHER
+
+        val httpResponse = HttpResponse(unHandledStatusCode, Some(errorResponseJson), Map("CorrelationId" -> Seq(correlationId)))
+        val result = AmendDividendsHttpParser.amendHttpReads.read(PUT, "/test", httpResponse)
+        result shouldBe Left(expected)
+      }
+
+      "the http response contains a 500 with an error response body" in {
+        val errorResponseJson = Json.parse(
+          """
+            |{
+            |  "code": "SERVER_ERROR",
+            |  "reason": "some reason"
+            |}
+          """.
+            stripMargin)
+        val expected =  DesResponse(correlationId, GenericError(DownstreamError))
+
+        val httpResponse = HttpResponse(INTERNAL_SERVER_ERROR, Some(errorResponseJson), Map("CorrelationId" -> Seq(correlationId)))
+        val result = AmendDividendsHttpParser.amendHttpReads.read(PUT, "/test", httpResponse)
+        result shouldBe Left(expected)
+      }
+
+      "the http response contains a 503 with an error response body" in {
+        val errorResponseJson = Json.parse(
+          """
+            |{
+            |  "code": "SERVICE_UNAVAILABLE",
+            |  "reason": "some reason"
+            |}
+          """.
+            stripMargin)
+        val expected =  DesResponse(correlationId, GenericError(DownstreamError))
+
+        val httpResponse = HttpResponse(SERVICE_UNAVAILABLE, Some(errorResponseJson), Map("CorrelationId" -> Seq(correlationId)))
+        val result = AmendDividendsHttpParser.amendHttpReads.read(PUT, "/test", httpResponse)
+        result shouldBe Left(expected)
+      }
+    }
+
+    "return multiple errors" when {
+      "the http response contains a 400 with an error response body with multiple errors" in {
+        val errorResponseJson = Json.parse(
+          """
+            |{
+            |	"failures" : [
+            |    {
+            |      "code": "INVALID_NINO",
+            |      "reason": "some reason"
+            |    },
+            |    {
+            |      "code": "INVALID_TAXYEAR",
+            |      "reason": "some reason"
+            |    }
+            |  ]
+            |}
+          """.stripMargin)
+        val expected =  DesResponse(correlationId, MultipleErrors(Seq(MtdError("INVALID_NINO", "some reason"), MtdError("INVALID_TAXYEAR", "some reason"))))
+
+        val httpResponse = HttpResponse(BAD_REQUEST, Some(errorResponseJson), Map("CorrelationId" -> Seq(correlationId)))
+        val result = AmendDividendsHttpParser.amendHttpReads.read(POST, "/test", httpResponse)
+        result shouldBe Left(expected)
+      }
+
+      "the http response contains a 403 with an error response body with multiple errors" in {
+        val errorResponseJson = Json.parse(
+          """
+            |{
+            |	"failures" : [
+            |    {
+            |      "code": "NOT_FOUND_INCOME_SOURCE",
+            |      "reason": "some reason"
+            |    },
+            |    {
+            |      "code": "MISSING_GIFT_AID_AMOUNT",
+            |      "reason": "some reason"
+            |    }
+            |  ]
+            |}
+          """.stripMargin)
+        val expected =  DesResponse(correlationId,
+          MultipleErrors(Seq(MtdError("NOT_FOUND_INCOME_SOURCE", "some reason"), MtdError("MISSING_GIFT_AID_AMOUNT", "some reason"))))
+
+        val httpResponse = HttpResponse(FORBIDDEN, Some(errorResponseJson), Map("CorrelationId" -> Seq(correlationId)))
+        val result = AmendDividendsHttpParser.amendHttpReads.read(POST, "/test", httpResponse)
+        result shouldBe Left(expected)
+      }
+    }
+  }
+}
