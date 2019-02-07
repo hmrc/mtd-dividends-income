0394ffdd9707a6d79219552dcaed24e6436a49bc
Narendra Vijayarao <26090262+narendravijayarao@users.noreply.github.com>
2019-02-07 16:15:02 +0000

    Created parser and validator for retrieve dividends

---
+++ test/v2/controllers/requestParsers/RetrieveDividendsRequestDataParserSpec.scala
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.controllers.requestParsers
+
+import support.UnitSpec
+import uk.gov.hmrc.domain.Nino
+import v2.mocks.validators.MockRetrieveDividendsValidator
+import v2.models.errors.{BadRequestError, ErrorWrapper, NinoFormatError, TaxYearFormatError}
+import v2.models.requestData.{DesTaxYear, RetrieveDividendsRequest, RetrieveDividendsRequestRawData}
+
+class RetrieveDividendsRequestDataParserSpec extends UnitSpec{
+
+  trait Test extends MockRetrieveDividendsValidator {
+    val parser = new RetrieveDividendsRequestDataParser(mockRetrieveDividendsValidator)
+  }
+
+  "Calling parse" should {
+    "return valid retrieve request object" when {
+      "valid request data is supplied" in new Test {
+        val nino = "AA123456A"
+        val taxYear = "2017-18"
+        val inputRawRequest = RetrieveDividendsRequestRawData(nino, taxYear)
+        val expectedResult = RetrieveDividendsRequest(Nino(nino), DesTaxYear(taxYear))
+
+        MockRetrieveDividendsValidator.validate(inputRawRequest).returns(Nil)
+
+        val result = parser.parse(inputRawRequest)
+
+        result shouldBe Right(expectedResult)
+      }
+    }
+
+    "return single validation nino format error" when {
+      "invalid nino and valid tax year is supplied" in new Test {
+        val nino = "AA1256A"
+        val taxYear = "2017-18"
+        val inputRawRequest = RetrieveDividendsRequestRawData(nino, taxYear)
+        val expectedResult = ErrorWrapper(None, NinoFormatError, None)
+
+        MockRetrieveDividendsValidator.validate(inputRawRequest).returns(List(NinoFormatError))
+        val result = parser.parse(inputRawRequest)
+
+        result shouldBe Left(expectedResult)
+      }
+    }
+
+    "return single validation tax year format error" when {
+      "invalid tax year and valid nino is supplied" in new Test {
+        val nino = "AA123456A"
+        val taxYear = "2017"
+        val inputRawRequest = RetrieveDividendsRequestRawData(nino, taxYear)
+        val expectedResult = ErrorWrapper(None, TaxYearFormatError, None)
+
+        MockRetrieveDividendsValidator.validate(inputRawRequest).returns(List(TaxYearFormatError))
+        val result = parser.parse(inputRawRequest)
+
+        result shouldBe Left(expectedResult)
+      }
+    }
+
+    "return multiple validation errors" when {
+      "invalid tax year and nino is supplied" in new Test {
+        val nino = "AA1256A"
+        val taxYear = "2017"
+        val inputRawRequest = RetrieveDividendsRequestRawData(nino, taxYear)
+        val expectedResult = ErrorWrapper(None, BadRequestError, Some(List(NinoFormatError, TaxYearFormatError)))
+
+        MockRetrieveDividendsValidator.validate(inputRawRequest).returns(List(NinoFormatError, TaxYearFormatError))
+        val result = parser.parse(inputRawRequest)
+
+        result shouldBe Left(expectedResult)
+      }
+    }
+  }
+}
---
+++ test/v2/mocks/validators/MockRetrieveDividendsValidator.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.mocks.validators
+
+import org.scalamock.handlers.CallHandler
+import org.scalamock.scalatest.MockFactory
+import v2.controllers.requestParsers.validators.RetrieveDividendsValidator
+import v2.models.errors.MtdError
+import v2.models.requestData.RetrieveDividendsRequestRawData
+
+class MockRetrieveDividendsValidator extends MockFactory {
+
+  val mockRetrieveDividendsValidator: RetrieveDividendsValidator = mock[RetrieveDividendsValidator]
+
+  object MockRetrieveDividendsValidator {
+    def validate(data: RetrieveDividendsRequestRawData): CallHandler[List[MtdError]] = {
+      (mockRetrieveDividendsValidator.validate(_: RetrieveDividendsRequestRawData))
+        .expects(data)
+    }
+  }
+
+}
