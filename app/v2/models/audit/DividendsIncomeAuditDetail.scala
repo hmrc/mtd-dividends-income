cc973083a5187e0ff3236e630897a171e25e4411
Narendra Vijayarao <26090262+narendravijayarao@users.noreply.github.com>
2019-03-06 17:18:41 +0000

    Updated audit detail request as jsvalue

--- app/v2/models/audit/DividendsIncomeAuditDetail.scala
+++ app/v2/models/audit/DividendsIncomeAuditDetail.scala
@@ -24,7 +24,7 @@ case class DividendsIncomeAuditDetail(
                                        agentReferenceNumber: Option[String],
                                        nino: String,
                                        taxYear: String,
-                                       request: Option[Dividends],
+                                       request: JsValue,
                                        `X-CorrelationId`: String,
                                        response: Option[AuditResponse] = None
                                      )
