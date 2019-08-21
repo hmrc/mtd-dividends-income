efddddfd9784636fcc27cfa9139aab2e7e7147c9
daniel-andrews1994 <38686400+daniel-andrews1994@users.noreply.github.com>
2019-08-21 16:53:30 +0100

    Updated dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,10 +22,10 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "0.36.0",
-    "uk.gov.hmrc" %% "domain" % "5.6.0-play-26",
-    "uk.gov.hmrc"   %% "play-hmrc-api" % "3.4.0-play-26",
-    "org.typelevel" %% "cats-core" % "1.6.0"
+    "uk.gov.hmrc"   %% "bootstrap-play-26" % "0.36.0",
+    "uk.gov.hmrc"   %% "domain"            % "5.6.0-play-26",
+    "uk.gov.hmrc"   %% "play-hmrc-api"     % "3.4.0-play-26",
+    "org.typelevel" %% "cats-core"         % "1.6.0"
   )
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
